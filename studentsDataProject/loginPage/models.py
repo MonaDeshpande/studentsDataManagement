cdfe973522c26ab427069414d1e5f604d92c3332
MonaDeshpande <101972805+MonaDeshpande@users.noreply.github.com>
2023-10-04 12:26:52 +0530

    commit

--- studentsDataProject/loginPage/models.py
+++ studentsDataProject/loginPage/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length= 200)
+    last_name =  models.CharField(max_length= 200) 
+    emailId =  models.CharField(max_length= 200)
+    password =  models.CharField(max_length= 200)
--- studentsDataProject/studentsDataProject/urls.py
+++ studentsDataProject/studentsDataProject/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('', include('loginPage.urls')),
+    path('website', include('website.urls')),
     path('admin/', admin.site.urls),
 ]
