605ff683fb09b05db18b17d56647a60fb0516144
MonaDeshpande <101972805+MonaDeshpande@users.noreply.github.com>
2023-10-09 12:15:21 +0530

    commit

--- studentsDataProject/studentsDataProject/urls.py
+++ studentsDataProject/studentsDataProject/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from loginPage import views
 
 urlpatterns = [
-    path('', include('loginPage.urls')),
+    path('loginpage', include('loginPage.urls')),
     path('website', include('website.urls')),
     path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
 ]
