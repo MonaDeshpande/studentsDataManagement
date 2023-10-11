702d206631dff2c3325469282bcb4bc7f571d175
MonaDeshpande <101972805+MonaDeshpande@users.noreply.github.com>
2023-10-11 13:26:25 +0530

    commit

--- studentsDataProject/studentsDataProject/urls.py
+++ studentsDataProject/studentsDataProject/urls.py
@@ -14,14 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from .views import*
-#from loginPage import views
+from django.urls import path, include
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("/",home),
-    path("loginpage/", loginPage),
-    path("signuppage/", signupPage),
-    #path('/', admin.site.urls),
+    path("index/", test),
 ]
--- studentsDataProject/studentsDataProject/views.py
+++ studentsDataProject/studentsDataProject/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 #Create your views here.
-def home(request):
-    return HttpResponse("Welcome to our site")
+def test(request):
+    print("Welcome to our site")
+    return HttpResponse("<h1> This is Index Page<h1>")
+
 def loginPage(request):
     return render(request, "login.html", {})
 
